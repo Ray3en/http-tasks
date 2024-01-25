a247e93b986dfda8abd7da9f0c6293b7dc948f7f
Papakhin Alexander <alexandr.papakhin@poleinvest.ru>
2024-01-25 13:42:17 +0700

    Добавил кастомную ручку для использования в разных примерах в тринкете

--- controllers/exampleController.js
+++ controllers/exampleController.js
@@ -26,6 +26,22 @@ class ExampleController{
             res.status(200).json({ estimatedDeliveryDate: estimatedDeliveryDate.toISOString().split('T')[0], orderId: req.query.orderId });
         }
     }
+
+    customRoute(req, res) {
+        const errorChance = req.body.errorChance || 0;
+        const errorCode = req.body.errorCode || 500;
+        const errorText = req.body.errorText || 'Что-то пошло не так...';
+        const responseBody = req.body.responseBody || {};
+        const delay = req.body.delay || 0;
+
+        setTimeout(() => {
+            if (randomError(errorChance)) {
+                res.status(errorCode).json({ error: errorText });
+            } else {
+                res.status(200).json(responseBody);
+            }
+        }, delay);
+    }
 }
 
 module.exports = new ExampleController()
